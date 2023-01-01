41c594093648720634ae263ce758b585d497f096
Tobias-Bonsack <tobias.bonsack@gmail.com>
2023-01-01 11:08:12 +0100

    resolve warnings etc

--- AgeInMinutes/src/main/java/com/example/ageinminutes/AgeInMinuteMain.kt
+++ AgeInMinutes/src/main/java/com/example/ageinminutes/AgeInMinuteMain.kt
@@ -1,25 +1,23 @@
 package com.example.ageinminutes
 
-import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.abs
 
 class AgeInMinuteMain : AppCompatActivity() {
 
-    lateinit var datePickerFragment: DatePickerFragment
+    private lateinit var datePickerFragment: DatePickerFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_age_in_minute_main)
         datePickerFragment = DatePickerFragment(this, Calendar.getInstance(), ::updateDate)
 
-        findViewById<Button>(R.id.selectDateBtn).setOnClickListener(View.OnClickListener { view ->
+        findViewById<Button>(R.id.selectDateBtn).setOnClickListener {
             datePickerFragment.show()
-        })
+        }
     }
 
     private fun updateDate() {
@@ -27,7 +25,7 @@ class AgeInMinuteMain : AppCompatActivity() {
         calculateMinutes()
     }
 
-    private fun calculateMinutes(): Unit {
+    private fun calculateMinutes() {
         val minuteTV = findViewById<TextView>(R.id.textView6)
         val cMinute = (Calendar.getInstance().timeInMillis / 60000).toInt()
         val theDate = abs(cMinute - (datePickerFragment.date.timeInMillis / 60000).toInt())

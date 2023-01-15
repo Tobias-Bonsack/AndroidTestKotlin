c06a71a16ba6774abba4f0591d2c69cb0afe351e
Tobias-Bonsack <tobias.bonsack@gmail.com>
2023-01-15 14:02:36 +0100

    start room demo

--- app/src/main/java/com/example/androidtestkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidtestkotlin/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.androidtestkotlin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ageinminutes.AgeInMinuteMain
+import com.example.calculator.CalculatorMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.ageBtn).setOnClickListener(View.OnClickListener {
+        findViewById<Button>(R.id.ageBtn).setOnClickListener {
             startActivity(Intent(applicationContext, AgeInMinuteMain::class.java))
-        })
+        }
+
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            startActivity(Intent(this, CalculatorMainActivity::class.java))
+        }
     }
 }
